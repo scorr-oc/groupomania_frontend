4c2988141638895b7d12a40b870a46aeae2537b2
scorr-oc <79743073+scorr-oc@users.noreply.github.com>
2022-07-14 12:24:17 +0200

    debut frontend

---
+++ groupomania_frontend_react/src/components/Banner/index.jsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/logo_svg.svg'
+import { Container } from '../../Utils/styles/bannerStyle'
+import { Logo } from '../../Utils/styles/bannerStyle'
+import { Styledlinked } from '../../Utils/styles/bannerStyle'
+
+function Banner() {
+  return (
+    <Container>
+      <Logo src={logo} alt="logo Groupomania" />
+      <nav>
+        <Styledlinked to="/signup">S'inscrire</Styledlinked>
+        <Styledlinked to="/login">Se connecter</Styledlinked>
+        <Styledlinked to="/login">Déconnexion</Styledlinked>
+      </nav>
+    </Container>
+  )
+}
+
+export default Banner
