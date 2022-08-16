cf5319783937181c3a68b21d31c4fea610b36926
Stephanie Corrias <scminos@gmail.com>
2022-08-16 16:57:06 +0200

    mise à jour P7

--- groupomania_backend/controllers/post.js
+++ groupomania_backend/controllers/post.js
@@ -5,22 +5,21 @@ const Post = require('../models/Post')
 // Importation de la BDD
 const mysqlconnection= require('../database/db')
 
-
-
 exports.createPost = (req, res) => {
-delete req.body.id
-// On récupère les informations du frontend à envoyer dans la BDD
-const title= req.body.title
-const description = req.body.description
-const image = req.body.image
-const createPost = new Post (title,description,image)
+  delete req.body.id
+  // On récupère les informations du frontend à envoyer dans la BDD
+  const title= req.body.title
+  const description = req.body.description
+  const image = req.body.image
+  const createPost = new Post(title, description, image, req.user.userId)
 
 // On enregistre les informations dans la BDD
 mysqlconnection.query(
-  'INSERT INTO post SET ?', createPost, (error) => {
+  'INSERT INTO post SET ?', createPost, (error,results) => {
     if(error){
       res.status(400).json({error});
     }else {
+  
       res.status(201).json({message:'Post enregistré!',createPost})
   }
   }
@@ -45,9 +44,13 @@ exports.getOnePost = (req, res) => {
     'SELECT * FROM post WHERE idpost= ?',id,
   (error,results) => {
     if(error){
-      res.status(404).json({error})
+      res.status(500).json({error})
     }else {
-      res.status(200).json(results)
+     const post = results && results[0]
+     if(!post){ 
+      return res.status(404).json({ message: 'le post n\'existe pas' })
+     }
+      res.status(200).json(post)
     }
   })
 }
@@ -62,7 +65,7 @@ exports.modifyPost = (req, res) => {
   const sqlUpdate = 'UPDATE post SET title = ?, description =?, image = ? WHERE idpost = ? '
 
   mysqlconnection.query(
-    sqlUpdate,[title,description,id], (error,results) =>{
+    sqlUpdate,[title,description,image,id], (error,results) =>{
       if(error){
         res.status(404).json({error})
       }else {
@@ -90,64 +93,16 @@ mysqlconnection.query(
 
 exports.likePost = (req, res) => {
   
-  // const userLiking = req.body.userLiking
-  // const postId = req.body.postId
-  // const sqlLike = 'INSERT INTO likes (userliking, postid) VALUES (?,?)'
+  const userLiking = req.body.userLiking
+  const postId = req.body.postId
+  const sqlLike = 'INSERT INTO likes (userliking, postid) VALUES (?,?)'
 
-  // mysqlconnection.query(
-  //   sqlLike,[userLiking, postId],(err, results)=> {
-  //     if(error){
-  //       res.status(404).json({error})
-  //     }else {
-  //       res.status(200).json({message: "post liké !"})
-  //     }
-  //   })
-
-//   Sauce.findOne({_id: req.params.id})
-//   .then (sauce => {
-//     if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1){
-//       Sauce.updateOne(
-//         { _id : req.params.id},
-//         { $inc: {likes: 1},
-//           $push: {usersLiked: req.body.userId}
-//         }
-//       )
-//       .then(() => res.status(201).json({message: 'like +1'}))
-//       .catch (error => res.status(400).json({error}))
-//     }
-
-//     if (sauce.usersLiked.includes(req.body.userId) && req.body.like === 0){
-//       Sauce.updateOne(
-//         { _id : req.params.id},
-//         { $inc: {likes: -1},
-//           $pull: {usersLiked: req.body.userId}
-//         }
-//       )
-//       .then(() => res.status(201).json({message: 'likes 0'}))
-//       .catch (error => res.status(400).json({error}))
-//     }
-
-//     if (!sauce.usersDisliked.includes(req.body.userId) && req.body.like === -1){
-//       Sauce.updateOne(
-//         { _id : req.params.id},
-//         { $inc: {dislikes: 1},
-//           $push: {usersDisliked: req.body.userId}
-//         }
-//       )
-//       .then(() => res.status(201).json({message: 'dislikes +1'}))
-//       .catch (error => res.status(400).json({error}))
-//     }
-
-//     if (sauce.usersDisliked.includes(req.body.userId) && req.body.like === 0){
-//       Sauce.updateOne(
-//         { _id : req.params.id},
-//         { $inc: {dislikes: -1},
-//           $pull: {usersDisliked: req.body.userId}
-//         }
-//       )
-//       .then(() => res.status(201).json({message: 'dislike 0'}))
-//       .catch (error => res.status(400).json({error}))
-//     }
-//   })
-//   .catch(error => res.status(404).json({error}))
+  mysqlconnection.query(
+    sqlLike,[userLiking, postId],(err, results)=> {
+      if(err){
+        res.status(404).json({error})
+      }else {
+        res.status(200).json({message: "post liké !"})
+      }
+    })
 }
